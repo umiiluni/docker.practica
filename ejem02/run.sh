8396aafaf63f986eb36161bc139a075e0190f71c
Uma Rovetta <umiluni4@gmail.com>
2026-05-26 17:12:47 -0300

    Implementación de ejercicios 1, 2, 3 y 7

---
+++ ejem02/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+docker run -d --name wordpress-db \
+    --mount source=wordpress-db,target=/var/lib/mysql \
+    -e MYSQL_ROOT_PASSWORD=secret \
+    -e MYSQL_DATABASE=wordpress \
+    -e MYSQL_USER=manager \
+    -e MYSQL_PASSWORD=secret \
+    mariadb:10.3.9
+docker run -d --name wordpress \
+    --link wordpress-db:mysql \
+    --mount type=bind,source="$(pwd)"/wordpress,target=/var/www/html \
+    -e WORDPRESS_DB_USER=manager \
+    -e WORDPRESS_DB_PASSWORD=secret \
+    -p 8080:80 \
+    wordpress:4.9.8
---
+++ ejem07/code/myapp/index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h1>LEMP Stack funcionando con PHP 8.2</h1>";
+phpinfo();
+?>
